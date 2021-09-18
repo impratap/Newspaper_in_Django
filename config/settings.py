ff32ab1be094d32209154e5511e5de7f9d878f90
Ankit pratap singh <33418077+impratap@users.noreply.github.com>
2021-09-18 22:49:20 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
+#STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
